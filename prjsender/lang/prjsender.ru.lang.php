14f78dea72c0c456b8873b5c152bd97ef8dbc855
devkont <yusupovb@gmail.com>
2014-08-07 16:45:55 +0400

    Загрузка исходных файлов

---
+++ prjsender/lang/prjsender.ru.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['prjsender'] = 'Подписка на проекты';
+$L['prjsender_title'] = 'Настройка подписки';
+$L['prjsender_desc'] = 'Пожалуйста, отметьте категории проектов для подписки:';
+
+$L['prjsender_mail_title'] = 'Новые проекты';
---
+++ prjsender/prjsender.setup.php
@@ -0,0 +1,28 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Code=prjsender
+Name=Prjsender
+Category=
+Description=Projects sender
+Version=1.0.0
+Date=2014-01-20
+Author=CMSWorks Team
+Copyright=CMSWorks Team 2010-2014
+Notes=BSD License
+SQL=
+Auth_guests=R
+Lock_guests=12345A
+Auth_members=RW
+Lock_members=12345A
+Requires_plugins=
+[END_COT_EXT]
+
+[BEGIN_COT_EXT_CONFIG]
+
+[END_COT_EXT_CONFIG]
+==================== */
+
+defined('COT_CODE') or die('Wrong URL');
+
+?>
